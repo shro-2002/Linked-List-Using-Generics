c84ba0496e27271471a1a04f0f8c92843a905997
shro-2002 <96790181+shro-2002@users.noreply.github.com>
2023-11-28 19:55:09 +0530

    Added UC-4

--- src/main/java/com/bridgelabs/LinkedListGenerics/LinkedList.java
+++ src/main/java/com/bridgelabs/LinkedListGenerics/LinkedList.java
@@ -33,6 +33,29 @@ public class LinkedList<T extends Comparable<T>> {
 	}
 
 	/*
+	 * @params: T data
+	 * 
+	 * @return : void
+	 * 
+	 * @Description: Inserting a new node at the end of the Linked List
+	 */
+
+	public void insertBetween(T data, T val) {
+		Node<T> newNode = new Node<T>(data);
+		if (head == null) {
+			head = newNode;
+			return;
+		}
+		Node<T> temp = head;
+		while (temp.data != val) {
+			temp = temp.next;
+		}
+		newNode.next = temp.next;
+		temp.next = newNode;
+
+	}
+
+	/*
 	 * @params: void
 	 * 
 	 * @return : void
@@ -86,6 +109,13 @@ public class LinkedList<T extends Comparable<T>> {
 		list.insertAtEnd(30);
 		list.insertAtEnd(70);
 		list.display();
+		System.out.println();
+
+		// Insert in Between
+		System.out.println("Insert in Between");
+		list.insertBetween(30, 56);
+		list.display();
+		System.out.println();
 
 	}
 
